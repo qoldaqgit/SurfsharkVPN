48795fa9689dc208273bd6e3684d43d33a27f06e
qoldaqgit <152833825+qoldaqgit@users.noreply.github.com>
2024-02-08 08:18:07 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,33 +1,35 @@
 #!/bin/bash
-sudo apt-get install openvpn -y
-sudo apt-get install unzip -y
-sudo apt install iptables -y
-cd /etc/openvpn
-sudo wget https://my.surfshark.com/vpn/api/v1/server/configurations
-sudo unzip configurations
+
+#update and install requirements
+sudo apt update &&
+sudo apt upgrade -y  &&
+sudo apt-get install openvpn -y &&
+sudo apt-get install unzip -y &&
+sudo apt install iptables -y &&
+sudo apt install resolvconf -y &&
+
+cd /etc/openvpn &&
+sudo wget https://my.surfshark.com/vpn/api/v1/server/configurations &&
+sudo unzip configurations &&
 
 #Configure the desired location to connect to
-echo sudo openvpn --config "/etc/openvpn/us-mia.prod.surfshark.com_udp.ovpn" --auth-user-pass /etc/openvpn/auth.txt >> connect.sh
-echo [username] >> auth.txt
-echo [password] >> auth.txt
+echo sudo openvpn --config "/etc/openvpn/us-mia.prod.surfshark.com_udp.ovpn" --auth-user-pass /etc/openvpn/auth.txt >> connect.sh &&
+echo [username] >> auth.txt &&
+echo [password] >> auth.txt &&
 
 #Enable IP Forwarding
-sudo sh -c "echo 1 > /proc/sys/net/ipv4/ip_forward"
+sudo sh -c "echo 1 > /proc/sys/net/ipv4/ip_forward" &&
 
 #Get the iptables config
-sudo wget https://raw.githubusercontent.com/qoldaqgit/SurfsharkVPN/main/iptables.sh
+sudo wget https://raw.githubusercontent.com/qoldaqgit/SurfsharkVPN/main/iptables.sh &&
 
 #Point DNS to Surfshark's
-sudo apt update
-sudo apt upgrade -y 
-sudo apt install resolvconf -y
-sudo echo nameserver 162.252.172.57 >> /etc/resolvconf/resolv.conf.d/head
-sudo echo nameserver 149.154.159.92 >> /etc/resolvconf/resolv.conf.d/head
-
-sudo resolvconf --enable-updates
-sudo resolvconf -u
-sudo systemctl restart resolvconf.service
-sudo systemctl restart systemd-resolved.service
+sudo echo nameserver 162.252.172.57 >> /etc/resolvconf/resolv.conf.d/head &&
+sudo echo nameserver 149.154.159.92 >> /etc/resolvconf/resolv.conf.d/head &&
+sudo resolvconf --enable-updates &&
+sudo resolvconf -u &&
+sudo systemctl restart resolvconf.service &&
+sudo systemctl restart systemd-resolved.service &&
 
 echo ###########################################
 echo ### Run sudo nano /etc/openvpn/auth.txt ###
