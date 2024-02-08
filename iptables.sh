0489ee4e6d0367f0af1e7d50abacb8816d5b0e36
qoldaqgit <152833825+qoldaqgit@users.noreply.github.com>
2024-02-08 07:42:30 -0500

    Config iptables to prevent transmit without vpn

---
+++ iptables.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+# Flush
+iptables -t nat -F
+iptables -t mangle -F
+iptables -F
+iptables -X
+
+
+# Block All
+iptables -P OUTPUT DROP
+iptables -P INPUT DROP
+iptables -P FORWARD DROP
+
+
+# allow Localhost
+iptables -A INPUT -i lo -m comment --comment "Allow Localhost IN" -j ACCEPT
+iptables -A OUTPUT -o lo -m comment --comment "Allow Localhost Out" -j ACCEPT
+
+
+# Make sure you can communicate with Surfshark DHCP server
+iptables -A OUTPUT -d 162.252.172.57 -m comment --comment "Allow DNS Out"  -j ACCEPT
+iptables -A INPUT -s 162.252.172.57 -m comment --comment "Allow DNS In"  -j ACCEPT
+
+
+# Make sure that you can communicate within your own network
+iptables -A INPUT -s 10.10.10.0/24 -d 10.10.10.0/24 -m comment --comment "Allow LAN In" -j ACCEPT
+iptables -A OUTPUT -s 10.10.10.0/24 -d 10.10.10.0/24 -m comment --comment "Allow LAN Out" -j ACCEPT
+
+
+# Allow established sessions to receive traffic:
+iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+
+# Allow TUN
+iptables -A INPUT -i tun+ -j ACCEPT
+iptables -A FORWARD -i tun+ -j ACCEPT
+iptables -A FORWARD -o tun+ -j ACCEPT
+iptables -t nat -A POSTROUTING -o tun+ -j MASQUERADE
+iptables -A OUTPUT -o tun+ -j ACCEPT
+
+
+# allow VPN connection
+iptables -I OUTPUT 1 -p udp --destination-port 1194 -m comment --comment "Allow VPN connection" -j ACCEPT
+
+
+# Block All
+iptables -A OUTPUT -j DROP
+iptables -A INPUT -j DROP
+iptables -A FORWARD -j DROP
+
+
+# Log all dropped packages, debug only.
+
+
+iptables -N logging
+iptables -A INPUT -j logging
+iptables -A OUTPUT -j logging
+iptables -A logging -m limit --limit 2/min -j LOG --log-prefix "IPTables general: " --log-level 7
+iptables -A logging -j DROP
+
+
+echo "saving"
+iptables-save > /etc/iptables.rules
+echo "done"
+#echo 'openVPN - Rules successfully applied, we start "watch" to verify IPtables in realtime (you can cancel it as usual CTRL + c)'
+#sleep 3
+#watch -n 0 "sudo iptables -nvL"
