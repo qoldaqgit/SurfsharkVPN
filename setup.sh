ae7c6d8114d745eeb35b067ee87535f7e347df87
qoldaqgit <152833825+qoldaqgit@users.noreply.github.com>
2024-02-13 22:57:35 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -26,7 +26,9 @@ echo [password] >> auth.txt &&
 sudo sh -c "echo 1 > /proc/sys/net/ipv4/ip_forward" &&
 
 #Get the iptables config
-sudo wget https://raw.githubusercontent.com/qoldaqgit/SurfsharkVPN/main/iptables.sh &&
+sudo wget https://raw.githubusercontent.com/qoldaqgit/SurfsharkVPN/main/iptables.sh ~/ &&
+sudo wget https://raw.githubusercontent.com/qoldaqgit/SurfsharkVPN/main/iptablesconfig.sh ~/ &&
+
 
 #Point DNS to Surfshark's
 sudo echo nameserver 162.252.172.57 >> /etc/resolvconf/resolv.conf.d/head &&
@@ -36,29 +38,15 @@ sudo resolvconf -u &&
 sudo systemctl restart resolvconf.service &&
 sudo systemctl restart systemd-resolved.service &&
 
-h=`hostname -I`
 
-f=`echo ${h} | cut -d '.' -f 1` &&
-s=`echo ${h} | cut -d '.' -f 2` &&
-t=`echo ${h} | cut -d '.' -f 3` &&
-
-l=$(echo  $f.$s.$t.0) &&
-
-sed  -e "s/networkIP/"$l"/g" iptables.sh >> iptables.sh &&
-
-if [[ $# -gt 0 ]]
-then
-sed  -e "s/#iptables/iptables/g" iptables.sh >> iptables.sh &&
-sed  -e "s/managementIP/"$1"/g" iptables.sh >> iptables.sh &&
-fi
+sudo bash ~/iptablesconfig.sh &&
 
 
-
-echo "###########################################" &&
-echo "### Run sudo nano /etc/openvpn/auth.txt ###" &&
-echo "###########################################" &&
-echo "###     and insert your credentials     ###" &&
-echo "###########################################"
+#echo "###########################################" &&
+#echo "### Run sudo nano /etc/openvpn/auth.txt ###" &&
+#echo "###########################################" &&
+#echo "###     and insert your credentials     ###" &&
+#echo "###########################################"
 
 #Run the following to test
 #sudo bash /etc/openvpn/iptables.sh && sleep 10 && sudo bash /etc/openvpn/connect.sh
